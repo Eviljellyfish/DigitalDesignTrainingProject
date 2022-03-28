c7b062c511636ffe1b9dfe1be4b9fe1ca19d83fe
Eviljellyfish <Eviljellyfish@rambler.ru>
2022-03-29 01:09:04 +0300

    Added service classes

--- ServiceModule/src/main/java/OrgStructureService.java
+++ ServiceModule/src/main/java/OrgStructureService.java
@@ -1,2 +1,40 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class OrgStructureService {
+
+    public boolean addOrg() {
+        throw new NotImplementedException();
+    }
+
+    public boolean deleteOrg(int id) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUOrg(int id) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUser(int id, String name) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUser(String name) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeOrgParent(int id, OrgStructure org) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeOrgHead(int orgId, int userId) {
+        throw new NotImplementedException();
+    }
+
+    public boolean addSubOrg(int orgId, int subOrgId) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeOrgName(int id, String name) {
+        throw new NotImplementedException();
+    }
 }
--- ServiceModule/src/main/java/UserService.java
+++ ServiceModule/src/main/java/UserService.java
@@ -1,2 +1,49 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class UserService {
+
+    public boolean addUser() {
+        throw new NotImplementedException();
+    }
+
+    public boolean deleteUser(int id) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUser(int id) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUser(int id, String name) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUser(String firstName, String secondName) {
+        throw new NotImplementedException();
+    }
+
+    public boolean findUser(int id, String firstName, String secondName) {
+        throw new NotImplementedException();
+    }
+
+    public boolean moveUserToOrg(int id, OrgStructure org) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeUserRole(int id, UserRoleEnum role) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeUserPosition(int id, String pos) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeUserFirstName(int id, String name) {
+        throw new NotImplementedException();
+    }
+
+    public boolean changeUserSecondName(int id, String name) {
+        throw new NotImplementedException();
+    }
+
 }
