970e2d38e59a583de221cabecd68ecce9c4ae88b
Eviljellyfish <Eviljellyfish@rambler.ru>
2022-04-08 08:51:48 +0300

    Fixed liquibase script and updated DBDiagram.png

--- dao-module/src/main/java/com/kashigin/stanislav/dao/UserDao.java
+++ dao-module/src/main/java/com/kashigin/stanislav/dao/UserDao.java
@@ -1,17 +1,55 @@
 package com.kashigin.stanislav.dao;
 
+import com.kashigin.stanislav.entity.OrgStructure;
 import com.kashigin.stanislav.entity.User;
+import com.kashigin.stanislav.entity.UserRoleEnum;
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao implements BaseDao<User> {
     @Override
     public User get(int id) {
-        return null;
+        User user = null;
+        try (Connection connection = DbConnection.getConnection()) {
+
+            PreparedStatement ps = connection.prepareStatement("select * from users where id = ?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                user = new User();
+                user.setFirstName(rs.getString(2));
+                user.setSecondName(rs.getString(3));
+                user.setRole(UserRoleEnum.valueOf(rs.getString(4)));
+                user.setOrg(rs.getObject(5, OrgStructure.class));
+                user.setPosition(rs.getString(6));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  user;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try (Connection connection = DbConnection.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from users");
+            while(rs.next()) {
+//                User users = new OrgStructure();
+//                users.setName(rs.getString(2));
+//                users.setHead(rs.getObject(3, User.class));
+//                users.setParent(rs.getObject(4, OrgStructure.class));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return  users;
     }
 
     @Override
