b5984af585ac77b4fc96c4402e6e71eb2a64bf91
Eviljellyfish <Eviljellyfish@rambler.ru>
2022-04-22 19:13:16 +0300

    Started spring context

--- dao-module/src/main/java/com/kashigin/stanislav/dao/repository/OrgStructureRepository.java
+++ dao-module/src/main/java/com/kashigin/stanislav/dao/repository/OrgStructureRepository.java
@@ -2,6 +2,8 @@ package com.kashigin.stanislav.dao.repository;
 
 import com.kashigin.stanislav.entity.OrgStructure;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrgStructureRepository extends CrudRepository<OrgStructure, Long> {
 }
--- dao-module/src/main/java/com/kashigin/stanislav/dao/repository/UserRepository.java
+++ dao-module/src/main/java/com/kashigin/stanislav/dao/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.kashigin.stanislav.dao.repository;
 
 import com.kashigin.stanislav.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
