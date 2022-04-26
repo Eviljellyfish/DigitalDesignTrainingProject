8481586f6324c7a87e8ed8b32e2442bd8cc784ec
Eviljellyfish <Eviljellyfishh@gmail.com>
2022-04-26 21:38:17 +0300

    Fixed some issues

--- controller-module/src/main/java/com/kashigin/stanislav/controller/UserAuthDataController.java
+++ controller-module/src/main/java/com/kashigin/stanislav/controller/UserAuthDataController.java
@@ -1,21 +1,11 @@
 package com.kashigin.stanislav.controller;
 
 import com.kashigin.stanislav.dto.UserAuthDataDto;
-import com.kashigin.stanislav.dto.UserDto;
 import com.kashigin.stanislav.dto.map.UserAuthDataMapper;
-import com.kashigin.stanislav.dto.map.UserMapper;
-import com.kashigin.stanislav.entity.UserAuthData;
-import com.kashigin.stanislav.entity.secutity.UserPrincipal;
 import com.kashigin.stanislav.service.UserAuthDataService;
-import com.kashigin.stanislav.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/v1/usersdata")
@@ -29,7 +19,7 @@ public class UserAuthDataController {
         this.userAuthDataMapper = userAuthDataMapper;
     }
 
-    //@PreAuthorize("hasAuthority('ADMIN')")
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping(consumes = "application/json")
     public UserAuthDataDto add(@RequestBody UserAuthDataDto user) {
         return userAuthDataMapper.convertToDto(userAuthDataService.add(userAuthDataMapper.convertToModel(user)));
