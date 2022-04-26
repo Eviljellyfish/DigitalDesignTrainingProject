4c418405bcc8aa0facabfe9f61591010cae4a54b
Eviljellyfish <Eviljellyfish@rambler.ru>
2022-04-26 12:06:37 +0300

    Added passswordEncoder

--- main-app-module/src/main/java/com/kashigin/stanislav/config/SecurityConfig.java
+++ main-app-module/src/main/java/com/kashigin/stanislav/config/SecurityConfig.java
@@ -1,17 +1,31 @@
 package com.kashigin.stanislav.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+                .authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 
 }
